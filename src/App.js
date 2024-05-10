9164dc4c8fc43f624639e010aac626ef75c88718
katofina <askantya@tut.by>
2024-05-10 20:53:55 +0300

    solve: add files, life cycle, childComponent, lesson_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Name from './name/Name';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    console.log("constructor");
+    super(props);
+    this.state = {input: ""};
+    console.log(this.state);  
+
+    this.onChange = this.onChange.bind(this);
+    this.handle = this.handle.bind(this);
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedStateFromProps");
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("didMount");
+  }
+
+  shouldComponentUpdate(){
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("didUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("willUnmount");
+  }
+  
+  render() {
+    console.log("render");
+    return <form onSubmit={this.handle}>
+      <Name user={"Андрей"}/>
+      <input type="text" placeholder="Text for sending" value={this.state.name} onChange={this.onChange}/>
+      <button>Отправить</button>
+    </form>
+  }
+
+  handle(e) {
+    e.preventDefault();
+    console.log("Value :" + this.state.input);
+  }
+
+  onChange(e) {
+    this.setState({input: e.target.value});
+  }
 }
 
 export default App;
---
+++ src/name/Name.jsx
@@ -0,0 +1,4 @@
+function Name(props) {
+    return <p>Привет: {props.user}</p>;
+};
+export default Name;
